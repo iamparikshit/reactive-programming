7d02bbb5e5bb8dfbfe36042ba3a4d971b3afac45
Parikshit Jagtap <parikshit.jagtap.20@gmail.com>
2022-10-17 22:58:15 +0530

    PRO-000| Parikshit | Initial commit

---
+++ src/main/kotlin/com/pariskhit/reactiveprogramming/ReactiveProgrammingApplication.kt
@@ -0,0 +1,11 @@
+package com.pariskhit.reactiveprogramming
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactiveProgrammingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactiveProgrammingApplication>(*args)
+}
---
+++ src/test/kotlin/com/pariskhit/reactiveprogramming/ReactiveProgrammingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pariskhit.reactiveprogramming
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveProgrammingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
