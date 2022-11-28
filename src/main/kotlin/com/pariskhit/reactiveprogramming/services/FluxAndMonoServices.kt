61f1c0bebd16fe0500201a9e946e807191980ea0
Parikshit Jagtap <parikshit.jagtap.20@gmail.com>
2022-11-28 22:05:55 +0530

    PRO-002 | Parikshit | Add filter function

--- src/main/kotlin/com/pariskhit/reactiveprogramming/services/FluxAndMonoServices.kt
+++ src/main/kotlin/com/pariskhit/reactiveprogramming/services/FluxAndMonoServices.kt
@@ -5,12 +5,41 @@ import reactor.core.publisher.Mono
 
 class FluxAndMonoServices {
     private val languages = listOf("Spanish", "French", "English", "Portuguese", "Russian", "German")
+    private val maxLengthOfLanguage = 6
 
-    fun languageFlux() : Flux<String>{
+    fun languageFlux(): Flux<String> {
         return Flux.fromIterable(languages).log()
     }
 
-    fun languageMono() : Mono<String> {
-        return Mono.just("Spanish").log()
+    fun languageMono(): Mono<List<String>> {
+        return Mono.just(listOf("Japanese", "Chinese")).log()
     }
+
+    fun languageFluxMap(): Flux<String> {
+        return Flux.fromIterable(languages)
+            .map { it.uppercase() }
+            .log()
+    }
+
+    fun languageMonoMap(): Mono<String?> {
+        return Mono.just(languages)
+            .map { it.firstOrNull()?.uppercase() }
+            .log()
+    }
+
+    fun languageFluxFilter(): Flux<String> {
+        return Flux.fromIterable(languages)
+            .filter { it.length > maxLengthOfLanguage }
+            .map { it.uppercase() }
+            .log()
+    }
+
+    fun languageMonoFilter(maxLength : Int  = maxLengthOfLanguage): Mono<String?> {
+        return Mono.just(languages)
+            .map { it.firstOrNull() }
+            .filter { it?.length?: 0 > maxLength }
+            .map { it?.uppercase() }
+            .log()
+    }
+
 }
--- src/test/kotlin/com/pariskhit/reactiveprogramming/services/FluxAndMonoServicesTest.kt
+++ src/test/kotlin/com/pariskhit/reactiveprogramming/services/FluxAndMonoServicesTest.kt
@@ -22,7 +22,52 @@ class FluxAndMonoServicesTest{
         val language = fluxAndMonoServices.languageMono()
 
         StepVerifier.create(language)
-            .expectNext("Spanish")
+            .expectNext(listOf("Japanese", "Chinese"))
+            .verifyComplete()
+    }
+
+    @Test
+    fun `should return flux of string with Map operator`() {
+        val languageFlux = fluxAndMonoServices.languageFluxMap()
+
+        StepVerifier.create(languageFlux)
+            .expectNext("SPANISH","FRENCH", "ENGLISH", "PORTUGUESE","RUSSIAN","GERMAN")
+            .verifyComplete()
+    }
+
+    @Test
+    fun `should return mono of string with map operator`() {
+        val language = fluxAndMonoServices.languageMonoMap()
+
+        StepVerifier.create(language)
+            .expectNext("SPANISH")
+            .verifyComplete()
+    }
+
+    @Test
+    fun `should retrun flux of string with filter operator`() {
+        val languageFlux = fluxAndMonoServices.languageFluxFilter()
+
+        StepVerifier.create(languageFlux)
+            .expectNext("SPANISH","ENGLISH", "PORTUGUESE","RUSSIAN")
+            .verifyComplete()
+    }
+
+    @Test
+    fun `should return mono of string with filter operator`() {
+        val language = fluxAndMonoServices.languageMonoFilter()
+
+        StepVerifier.create(language)
+            .expectNext("SPANISH")
+            .verifyComplete()
+    }
+
+    @Test
+    fun `should return nothing when it doesn't filter with give criteria`() {
+        val language = fluxAndMonoServices.languageMonoFilter(7)
+
+        StepVerifier.create(language)
+            .expectNext()
             .verifyComplete()
     }
 }
